558fb4a9bf8cc76df96fe6a4161d2c29138c4b13
Florence EXTRAT <florence@MacBook-Air-de-Florence.local>
2023-10-09 17:14:06 +0200

    feat: importation du AppLayout

--- resources/js/Pages/Courses/CoursesIndex.vue
+++ resources/js/Pages/Courses/CoursesIndex.vue
@@ -1,19 +1,34 @@
 <template>
-    <div>
-        <h1 id="h1">{{ message }}</h1>
-    </div>
+    <AppLayout title="Page des formations">
+         <!-- en-tête -->
+        <template #header>
+            <h1 id="h1">{{ message }}</h1>
+            <h2 id="h2">{{ sousMessage }} </h2>
+        </template>
+    </AppLayout>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import AppLayout from './../../Layouts/AppLayout.vue';
+
+const components = {
+    AppLayout
+};
 
 const message = ref('Salut Flo !');
+const sousMessage = ref('Bienvenue dans ton appli :)')
 </script>
 
 <style scoped>
-#h1{
+#h1 {
     font-size: 60px;
     text-align: center;
-    color:blueviolet;
+    color: blueviolet;
+}
+#h2 {
+    font-size: 30px;
+    text-align: center;
+    color: orchid;
 }
 </style>
