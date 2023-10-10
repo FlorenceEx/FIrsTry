4e68a28c67ccd22c55956320d54bbb6896a536e3
Florence EXTRAT <florence@MacBook-Air-de-Florence.local>
2023-10-10 16:27:13 +0200

    feat: un peu de style

--- resources/js/Pages/Courses/CoursesIndex.vue
+++ resources/js/Pages/Courses/CoursesIndex.vue
@@ -17,15 +17,17 @@ const props = defineProps({
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="bg-blue-200 rounded-3xl">
                     <div class="p-8" v-if="courses.length > 0">
-                        <ul>
-                            <li v-for="course in courses">
-                                {{ course.title }}
+                        <ul class="py-4">
+                            <li class="py-6 mx-8 px-6 my-2 bg-white rounded shadow" v-for="course in courses">
+                                <div class="hover:font-bold text-3xl text">{{ course.title }}</div>
+                                <div class="text-sm text-slate-700">{{ course.description }}</div>
+                                <!-- <div class="bg-sky-600 text-white px-2 py-2 mx-80 my-2 text-center hover:bg-sky-800">Voir la formation</div> -->
                             </li>
                         </ul>
                     </div>
-                    <div class="p-8" v-else>
+                    <div class="py-6 mx-8 px-6 my-2 bg-white rounded shadow" v-else>
                         Il n'y a aucune formation à afficher.
                     </div>
                 </div>
