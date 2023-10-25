5b84f021113fff8d9e83d7686f8a36007aced06a
Florence EXTRAT <florence@MacBook-Air-de-Florence.local>
2023-10-25 15:12:12 +0200

    feat: création table et données movies

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -12,7 +12,6 @@ class CourseFactory extends Factory
 {
     /**
      * Define the model's default state.
-     *
      * @return array<string, mixed>
      */
     public function definition(): array
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Movie;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -17,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             CourseSeeder::class,
+            MovieSeeder::class,
         ]);
     }
 }
