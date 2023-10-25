a19a8833e0a5915dfdb742c365ee3a11bc50bd07
Florence EXTRAT <florence@MacBook-Air-de-Florence.local>
2023-10-25 16:40:21 +0200

    feat: mise en place et test d'une fenêtre modale

--- resources/js/Pages/MovieTable.vue
+++ resources/js/Pages/MovieTable.vue
@@ -1,6 +1,9 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
+import Modal from '@/Components/Modal.vue';
+import { ref } from 'vue';
 
+const showModal = ref(false);
 const props = defineProps({
     movies: Array,
 })
@@ -19,23 +22,39 @@ const props = defineProps({
                 <table class="border-collapse table-auto w-full text-sm">
                     <thead>
                         <tr>
-                            <th class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                            <th
+                                class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
                                 Titre</th>
-                            <th class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                            <th
+                                class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
                                 Réalisateur</th>
-                            <th class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                            <th
+                                class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
                                 Synopsis</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr v-for="movie in movies">
-                            <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-600">{{ movie.titre }}</td>
-                            <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-600">{{ movie.realisateur }}</td>
-                            <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-600">{{ movie.synopsis }}</td>
+                            <td
+                                class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-600">
+                                {{ movie.titre }}</td>
+                            <td
+                                class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-600">
+                                {{ movie.realisateur }}</td>
+                            <td
+                                class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-600">
+                                {{ movie.synopsis }}</td>
                         </tr>
                     </tbody>
                 </table>
             </div>
         </div>
-    </AppLayout>
-</template>
+        <div>
+        <button @click="showModal = true">Ouvrir la fenêtre modale</button>
+        <Modal :show="showModal" @close="showModal = false">
+            <h3>Ma fenêtre modale</h3>
+            <p>Ceci est le contenu de ma fenêtre modale (j'ai écrit "fenêtre modale" beaucoup trop de fois en 5 lignes).
+            </p>
+        </Modal>
+    </div>
+</AppLayout></template>
