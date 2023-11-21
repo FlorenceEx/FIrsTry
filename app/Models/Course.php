5a1969431f201155306553be9dac5d3a0573b87b
Florence EXTRAT <florence@MacBook-Air-de-Florence.local>
2023-11-21 17:05:42 +0100

    feat: création table pivot élèves-courses

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -16,4 +16,9 @@ class Course extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function eleves()
+    {
+        return $this->belongsToMany(Eleve::class);
+    }
 }
